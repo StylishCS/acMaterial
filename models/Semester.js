c19c253cf247ee57a7ddaeba58e62299ffa89914
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-05-30 07:25:10 +0300

    modify semester issue

--- models/Semester.js
+++ models/Semester.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const semesterSchema = new mongoose.Schema(
   {
+    semesterNumber: {
+      type: Number,
+      required: true,
+      enum: [1, 2],
+    },
     courses: {
       type: [mongoose.Schema.Types.ObjectId],
       required: true,
